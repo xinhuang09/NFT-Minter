507ed6fcc3d98242195569b35f54655f375428bb
Richard <richard@Richards-MacBook-Pro.local>
2022-05-13 01:08:47 -0400

    message

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './styles/App.scss';
+import MainMint from './MainMint.jsx';
+import Navbar from './Navbar.jsx';
+
+import { useState } from "react";
+
+export default function App() {
+
+  const [accounts, setAccounts] = useState([]);
+
+  return (
+    <main className="overlay">
+      <section className="App">
+        <Navbar accounts={accounts} setAccounts={setAccounts} />
+        <MainMint accounts={accounts} setAccounts={setAccounts} />
+      </section>
+      <div className="Background"></div>
+    </main>
+  );
+}
